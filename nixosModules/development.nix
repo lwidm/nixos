74884eb912068cdd4ffc5b8ab7fa52e6e6481b09
lwidm <lukas@widmer.id.au>
2024-04-14 14:40:53 +0200

    first attempt to make development.nix toggelable

--- nixosModules/development.nix
+++ nixosModules/development.nix
@@ -1,13 +1,27 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [    # packages for i3
-    neovim
-    tmux
+# development.nix
 
-    (python3.withPackages (python-pkgs: [] ))
-    clang
-    gnumake
-    cargo
-    zip
-    unzip
-  ];
+{ pkgs, lib, config, ... }: {
+
+  options = {
+
+    development.enable = lib.mkEnableOption "enables development.nix";
+
+  };
+
+
+  config = lib.mkIf config.development.enable {
+
+    environment.systemPackages = with pkgs; [    # packages for i3
+      neovim
+      tmux
+
+      (python3.withPackages (python-pkgs: [] ))
+      clang
+      gnumake
+      cargo
+      zip
+      unzip
+    ];
+
+  };
 }
