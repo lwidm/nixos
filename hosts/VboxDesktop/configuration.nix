a72dc9fab840c8a94603e41bb0bb1b06ab2a247e
lwidm <lukas@widmer.id.au>
2024-04-27 11:42:10 +0200

    installed nix-prefetch

--- hosts/VboxDesktop/configuration.nix
+++ hosts/VboxDesktop/configuration.nix
@@ -48,6 +48,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [    
+    nix-prefetch
     firefox
     stow
     neofetch
