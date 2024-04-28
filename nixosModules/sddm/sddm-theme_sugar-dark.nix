8896bff769e3cf32de83b98b7500396daa7caf40
lwidm <lukas@widmer.id.au>
2024-04-28 11:57:18 +0200

    added img to sddm

--- nixosModules/sddm/sddm-theme_sugar-dark.nix
+++ nixosModules/sddm/sddm-theme_sugar-dark.nix
@@ -6,17 +6,17 @@ let
 	imgLink = "https://raw.githubusercontent.com/NixOs/nixos-artwork/master/wallpapers/nix-wallpaper-nineish-solarized-light.png";
 
 	image = pkgs.fetchurl {
-	  url = imgLink;
-	  sha256 = pkgs.lib.fakeSha256;
+		url = imgLink;
+		hash = "psha256-gb5s5ePdw7kuIL3SI8VVhOcLcHu0cHMJJ623vg1kz40=";
 	};
 in
 pkgs.stdenv.mkDerivation {
 	name = "sddm-theme_sugar-dark";
 	src = pkgs.fetchFromGitHub {
-	  owner = "MarianArlt";
-	  repo = "sddm-sugar-dark";
-	  rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
-	  hash = "sha256-flOspjpYezPvGZ6b4R/Mr18N7N3JdytCSwwu6mf4owQ=";
+		owner = "MarianArlt";
+		repo = "sddm-sugar-dark";
+		rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
+		hash = "sha256-flOspjpYezPvGZ6b4R/Mr18N7N3JdytCSwwu6mf4owQ=";
 	};
 	installPhase = ''
 		mkdir -p $out
