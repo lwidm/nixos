4ae0d826131830a2e93b42ec0d19d0c39fe23ee8
lwidm <lukas@widmer.id.au>
2024-04-29 17:45:55 +0200

    added locale settings

--- nixosModules/base-config.nix
+++ nixosModules/base-config.nix
@@ -17,9 +17,20 @@
     time.timeZone = "Europe/Zurich";
 
     # Select internationalisation properties.
-    i18n = {
-      defaultLocale = "en_AU.UTF-8";
+    i18n.defaultLocale = "en_AU.UTF-8";
+
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "de_CH.UTF-8";
+      LC_IDENTIFICATION = "de_CH.UTF-8";
+      LC_MEASUREMENT = "de_CH.UTF-8";
+      LC_MONETARY = "de_CH.UTF-8";
+      LC_NAME = "de_CH.UTF-8";
+      LC_NUMERIC = "de_CH.UTF-8";
+      LC_PAPER = "de_CH.UTF-8";
+      LC_TELEPHONE = "de_CH.UTF-8";
+      LC_TIME = "de_CH.UTF-8";
     };
+
     console = {
       # font = "Lat2-Terminus16";
       keyMap = "sg";
