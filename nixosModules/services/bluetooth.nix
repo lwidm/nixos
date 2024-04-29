62718d0898f12eed10e485c2cdc7346f7dd13ed2
lwidm <lukas@widmer.id.au>
2024-04-29 18:05:05 +0200

    added bluetooth

---
+++ nixosModules/services/bluetooth.nix
@@ -0,0 +1,18 @@
+# bluetooth.nix
+
+{ pkgs, lib, config, ... }: {
+
+	options = {
+		blueooth.enable = lib.mkEnableOption "enable bluetooth.nix"
+	};
+
+	config = lib.mkIf config.bluetooth.enable {
+
+		hardware.bluetooth.enable = true;
+		hardware.bluetooth.powerOnBoot = true;
+
+		services.blueman.enable = true;
+
+	};
+
+}
