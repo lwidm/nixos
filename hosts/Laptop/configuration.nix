8be1374c97b0ecfc60efae7ca146213e38b65f77
lwidm <lukas@widmer.id.au>
2024-05-10 13:25:20 +0200

    testing touchpad settings

--- hosts/Laptop/configuration.nix
+++ hosts/Laptop/configuration.nix
@@ -38,7 +38,7 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Touchpad settings
-  services.xserver.libinput.naturalScrolling = false;
+  services.xserver.libinput.naturalScrolling = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.lukas = {
