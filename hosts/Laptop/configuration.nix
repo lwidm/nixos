8316771def839cece41fbfcae0acabe471e896a6
lwidm <lukas@widmer.id.au>
2024-05-10 11:52:13 +0200

    added Laptop

---
+++ hosts/Laptop/configuration.nix
@@ -0,0 +1,115 @@
+# ./hosts/default/configuration.nix
+
+{ config, stdenv, lib, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  environment.variables = {
+    MYSYSTEM = "Desktop";
+  };
+
+  fonts.enable = true;
+  X11_i3_startx.enable = false;
+  hyprland.enable = true;
+  development.enable = true;
+  nvidia.enable = true;
+  sddm.enable = true;
+  bluetooth.enable = true;
+
+  wacom.enable = true;
+  steam.enable = true;
+
+
+  # Networking
+  networking =  {
+    hostName = "lwidmVboxNixos"; # Define your hostname.
+    networkmanager.enable = true;
+    # wireless.enable = true; # Enables wireless support via wpa_supplicant.
+  };
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true; 
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+
+  # Virtualbox
+  virtualisation.virtualbox.guest.enable = true;
+  # virtualisation.virtualbox.guest.x11 = true;
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.lukas = {
+    isNormalUser = true;
+    description = "lukas widmer";
+    useDefaultShell = true;
+    # WARNING: not sure whether it is smart to add the user to the input group
+    extraGroups = [ "wheel" "networkmanager" "input" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      polybar
+      firefox
+      gimp
+      obsidian
+    ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [    
+    nix-prefetch
+    nix-prefetch-git
+    nix-prefetch-github
+    neofetch
+    wdisplays
+  ];
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
+
