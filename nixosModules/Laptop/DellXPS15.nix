d33fe51a45b4bbeeb70059b96bff3eb9abdabb04
lwidm <lukas@widmer.id.au>
2024-05-10 16:22:15 +0200

    mistake

--- nixosModules/Laptop/DellXPS15.nix
+++ nixosModules/Laptop/DellXPS15.nix
@@ -10,33 +10,30 @@
 
 		thermald_tlp.enable = true;
 		
-		nvidia.configuration = { 
 
-			hardware.nvidia.prime = { 
-				offload = {
-					enable = true;
-					enableOffloadCmd = true;
-				};
-
-				# Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA 
-				nvidiaBusId = "PCI:01:00:0"; 
-
-				# Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA 
-				intelBusId = "PCI:00:02:0"; 
+		hardware.nvidia.prime = { 
+			offload = {
+				enable = true;
+				enableOffloadCmd = true;
 			};
 
-			specialization = {
-				dgpu.configuration = {
-					hardware.nvidia = {
-						prime.sync.enable = lib.mkForce true;
-						prime.offload = {
-							enable = lib.mkForce false;
-							enableOffloadCmd = lib.mkForce false;
-						};
+			# Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA 
+			nvidiaBusId = "PCI:01:00:0"; 
+
+			# Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA 
+			intelBusId = "PCI:00:02:0"; 
+		};
+
+		specialization = {
+			dgpu.configuration = {
+				hardware.nvidia = {
+					prime.sync.enable = lib.mkForce true;
+					prime.offload = {
+						enable = lib.mkForce false;
+						enableOffloadCmd = lib.mkForce false;
 					};
 				};
 			};
-						
 		};
 
 	};
