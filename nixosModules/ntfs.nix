6768af90e067fcb747aaeb37479baeefb13af53c
lwidm <lukas@widmer.id.au>
2024-05-11 11:58:06 +0200

    added ntfs module

---
+++ nixosModules/ntfs.nix
@@ -0,0 +1,18 @@
+# nixosModules/ntfs.nix
+
+{ pkgs, lib, config, ... }: {
+
+	options = {
+		ntfs.enable = lib.mkEnableOption "enables tools for reading and writing to ntfs file systems";
+	};
+
+	config = lib.mkIf config.ntfs.enable {
+
+		environment.systemPackages = with pkgs; [
+			fuse
+			ntfs-3g
+		];
+
+	};
+
+}
