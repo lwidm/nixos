06b61d149f9c9ad32c9d9548b6d2c0f61d772f67
lwidm <lukas@widmer.id.au>
2024-06-02 08:46:49 +0000

    installed ripgrep

--- nixosModules/development.nix
+++ nixosModules/development.nix
@@ -14,6 +14,7 @@
     environment.systemPackages = with pkgs; [    # packages for i3
       neovim
       tmux
+      ripgrep
 
       nodejs
       (python3.withPackages (python-pkgs: with python-pkgs; [
