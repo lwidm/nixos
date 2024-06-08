c50e3502232beb8fb2ee813ad7550d21123c2f92
lwidm <lukas@widmer.id.au>
2024-06-08 16:00:09 +0000

    permanetly add epson printer

--- nixosModules/services/printing.nix
+++ nixosModules/services/printing.nix
@@ -13,11 +13,24 @@
 
     services.avahi = {
       enable = true;
-      nssmdns = true;
+      nssmdns4 = true;
       openFirewall = true;
     };
 
     services.printing.drivers = with pkgs; [ epsonscan2 epson-escpr2 ];
+
+    hardware.printers = {
+      ensurePrinters = [{
+        name = "EPSON_ET_3850_Series";
+        location = "Home";
+        deviceUri =
+          "dnssd://EPSON%20ET-3850%20Series._ipp._tcp.local/?uuid=cfe92100-67c4-11d4-a45f-e0bb9e329d02";
+        model =
+          "epson-inkjet-printer-escpr2/Epson-ET-3850_Series-epson-escpr2-en.ppd";
+        ppdOptions = { PageSize = "A4"; };
+      }];
+    };
+
   };
 
 }
