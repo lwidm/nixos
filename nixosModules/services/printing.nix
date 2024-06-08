635eab16bb7493ea573f9de1a14460efd2e2a7fa
lwidm <lukas@widmer.id.au>
2024-06-08 18:45:14 +0200

    default printer

--- nixosModules/services/printing.nix
+++ nixosModules/services/printing.nix
@@ -21,7 +21,7 @@
 
     hardware.printers = {
       ensurePrinters = [{
-        name = "EPSON_ET_3850_Series";
+        name = "EPSON_ET-3850_Series";
         location = "Home";
         deviceUri =
           "dnssd://EPSON%20ET-3850%20Series._ipp._tcp.local/?uuid=cfe92100-67c4-11d4-a45f-e0bb9e329d02";
@@ -29,6 +29,7 @@
           "epson-inkjet-printer-escpr2/Epson-ET-3850_Series-epson-escpr2-en.ppd";
         ppdOptions = { PageSize = "A4"; };
       }];
+      ensureDefaultPrinter = "Epson-ET-3850_Series";
     };
 
     environment.systemPackages = with pkgs; [    # packages for i3
