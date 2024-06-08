cd2ad7491b5eba02e3c84fa73bc8611cb8dfced9
lwidm <lukas@widmer.id.au>
2024-06-08 10:01:27 +0000

    added printing and nixfmt

---
+++ nixosModules/printing.nix
@@ -0,0 +1,23 @@
+# nixosModules/printing.nix
+
+{ pkgs, lib, config, ... }: {
+
+  options = {
+
+    printing.enable = lib.mkEnableOption "enables printing.nix";
+
+  };
+
+  config = lib.mkIf config.printing.enable {
+    services.printing.enable = true;
+
+    services.avahi = {
+      enable = true;
+      nssmdns = true;
+      openFirewall = true;
+    };
+
+    services.printing.drivers = with pkgs; [ epsonscan2 peson-escpr2 ];
+  };
+
+}
--- shell.nix
+++ shell.nix
@@ -6,6 +6,7 @@ pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     nil
     nixpkgs-fmt
+    nixfmt
   ];
   shellHook = ''
     zsh
