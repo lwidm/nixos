837814716ea34d3ec9834ec74f21ba1b3731fb2f
lwidm <lukas@widmer.id.au>
2024-09-23 20:23:32 +0200

    small mistake in wsl-config

--- nixosModules/wsl-config.nix
+++ nixosModules/wsl-config.nix
@@ -8,15 +8,17 @@
   ...
 }:
 {
-
   options = {
-
     wsl-config.enable = lib.mkEnableOption "enables options shared among all my wsl configs";
   };
 
   config = lib.mkIf config.wsl-config.enable {
     environment.variables = rec {
-      WINHOME = "/mnt/c/Users/lukas";
+      WINHOME = "/mnt/c/Users/lukas/";
     };
+
+    environment.systemPackages = with pkgs; [
+      neofetch
+    ];
   };
 }
